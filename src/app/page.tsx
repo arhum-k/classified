04ca8d96d68dc1a711d36b318b55a7811e3edb75
arhum khan <arhum@berkeley.edu>
2024-07-17 18:08:56 -0400

    added button to route to display on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@
 import React, { useEffect, useState, useRef } from "react";
 import Navbar from "./components/Navbar";
 import CampusData from "./context/CampusData";
+import Link from "next/link";
 
 export default function Home() {  
   
   return (
     <CampusData>
       <Navbar/>
-      <div>HOME PAGE</div>
+      <Link href="/display/">
+        <button className="border border-black"> go to display page</button>
+      </Link>
     </CampusData>
   )
 }
